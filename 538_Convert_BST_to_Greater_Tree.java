44ad43f38f7f02fe8454829d9d278f0b4832277d
CHLee <jerrychlee@gmail.com>
2017-11-15 09:55:53 +0800

    Tree & DFS

--- 538_Convert_BST_to_Greater_Tree.java
+++ 538_Convert_BST_to_Greater_Tree.java
@@ -1,3 +1,38 @@
+/* Version 2.0 */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    int sum = 0;
+    
+    public TreeNode convertBST(TreeNode root) {
+        
+        if(root!=null){
+            if(root.right!=null){
+                convertBST(root.right);
+            }
+            
+            root.val = root.val+sum;
+            sum = root.val;
+            
+            if(root.left!=null){
+                convertBST(root.left);
+            }
+            
+        }
+        return root;
+    }
+    
+}
+
+/* Version 1.0 */
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
