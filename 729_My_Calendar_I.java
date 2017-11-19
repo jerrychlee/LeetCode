cf289b261cbba04a844dced65ac65587286002a1
CHLee <jerrychlee@gmail.com>
2017-11-19 16:26:51 +0800

    Pair & Booking

---
+++ 729_My_Calendar_I.java
@@ -0,0 +1,59 @@
+class MyCalendar {
+    
+    List<Pair<Integer,Integer>> event = null;
+    
+    public MyCalendar() {
+        event = new ArrayList<Pair<Integer,Integer>>();
+    }
+    
+    public boolean book(int start, int end) {
+        boolean isFree = true;
+        for(int i=0; i<event.size(); i++){
+            Pair p = event.get(i);
+            if((start>=(int)p.getLeft() && start<(int)p.getRight()) || (end>(int)p.getLeft() && end<=(int)p.getRight())
+               ||(start<=(int)p.getLeft() && end>=(int)p.getRight())){
+                isFree = false;
+                break;
+            }
+        }
+        
+        if(isFree==true){
+            event.add(new Pair(start,end));
+        }
+        
+        return isFree;
+    }
+    
+    /*******************************/
+    /*         Pair                */
+    /*******************************/
+    private class Pair<L,R> {
+        private final L left;
+        private final R right;
+        public Pair(L left, R right) {
+            this.left = left;
+            this.right = right;
+        }
+        
+        public L getLeft() { return left; }
+        public R getRight() { return right; }
+        
+        @Override
+        public int hashCode() { return left.hashCode() ^ right.hashCode(); }
+        
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Pair)) return false;
+            Pair pairo = (Pair) o;
+            
+            return this.left.equals(pairo.getLeft()) && this.right.equals(pairo.getRight());
+        }
+    }
+    
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
