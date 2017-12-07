e3a0ddc9959e676ec94e1c476003eeb55340c2b6
CHLee <jerrychlee@gmail.com>
2017-12-07 18:31:02 +0800

    Math

---
+++ 53_Maximum_Subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        
+        for(int i=0;i<nums.length;i++){
+            if(sum<0){
+                sum = nums[i];
+            }else{
+                sum+=nums[i];
+            }
+            max = Math.max(max,sum);
+        }        
+        return max;
+    }
+}
