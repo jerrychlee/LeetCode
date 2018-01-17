ee3f9f254d82531fb09e01a43b7c2be045f3de2b
CHLee <jerrychlee@gmail.com>
2018-01-17 10:19:04 +0800

    Math

---
+++ 453_Minimum_Moves_to_Equal_Array_Elements.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for(int num:nums){
+            sum+=num;
+            if(num<min){
+                min = num;
+            }
+        }
+        return sum - min*nums.length;
+    }
+}
