b5147bdbc8a133007fab6820eae84ceeb21eab92
CHLee <jerrychlee@gmail.com>
2019-02-14 16:01:25 +0800

    Deque & Stack & DFS & Inorder

---
+++ 94_Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,43 @@
+
+Binary Tree Inorder Traversal
+
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+ 
+class Solution {
+    List<Integer> orderList = new ArrayList();
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        Deque<TreeNode> stack = new ArrayDeque();
+        if(root == null){
+            return orderList;
+        }
+        
+        stack.add(root);
+        while (stack.size()!=0) {
+            TreeNode node = stack.peekFirst();
+            if(node.left == null){
+                orderList.add(node.val);
+                stack.pollFirst();
+                if(node.right != null){
+                    stack.addFirst(node.right);
+                }
+            }else{
+                stack.addFirst(node.left);
+                node.left = null; // Cool!
+            }
+        }
+    
+        return orderList;        
+    }
+}
