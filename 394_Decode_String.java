a5aefc22146b2cfa50ad9dfd0d3a9064830a8160
CHLee <jerrychlee@gmail.com>
2019-02-18 11:06:18 +0800

    Stack & Deque & StringBuilder & String

---
+++ 394_Decode_String.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String decodeString(String s) {
+        Deque<Integer> num = new ArrayDeque();
+        Deque<String> remain = new ArrayDeque();
+        StringBuilder out = new StringBuilder();
+            
+        char [] chars = s.toCharArray();
+        int i = 0;
+        int n = 0;
+        
+        while(i< chars.length){
+            if(Character.isDigit(chars[i])){
+                n = n*10 + chars[i]-'0';
+            }else if(Character.isLetter(chars[i])){
+                out.append(chars[i]);
+            }else{
+                
+                switch(chars[i]){
+                    case '[':
+                        num.offerLast(n);
+                        n = 0;
+                        remain.offerLast(out.toString());
+                        out.delete( 0, out.length() );
+                        break;
+                    case ']':
+                        StringBuilder temp2 = new StringBuilder();
+                        int repeatTimes = num.pollLast();
+                        temp2.append(remain.pollLast());
+                        for(int c=0 ; c<repeatTimes; c++){
+                           temp2.append(out.toString()); 
+                        }
+                        out = temp2;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            i++;        
+        }
+        
+        return out.toString();
