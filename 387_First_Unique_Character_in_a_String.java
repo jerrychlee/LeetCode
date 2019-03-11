9b837447dc68def3b44f7e72f5505e5e45457a5e
Chun-Hsien Lee <jerrychlee@gmail.com>
2019-03-11 12:20:13 +0800

    Hashmap & Array Counting

--- 387_First_Unique_Character_in_a_String.java
+++ 387_First_Unique_Character_in_a_String.java
@@ -1,4 +1,18 @@
-class Solution {    
+First Unique Character in a String
+
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+
+===============================================================
+class Solution1 {    
     public int firstUniqChar(String s) {
         int [] array = new int[26];
         for(int i=0; i<s.length(); i++){
@@ -10,5 +24,32 @@ class Solution {
                 return i;
         }
         return -1;
+    }
+}
+
+===============================================================
+class Solution2 {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            if(!map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i), i);
+            }else{
+                map.replace(s.charAt(i), Integer.MAX_VALUE);
+            }
+        }
+        
+        int out = Integer.MAX_VALUE;
+        for(Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if(out > entry.getValue()){
+                out = entry.getValue();
+            }
+        }
+        
+        if(out == Integer.MAX_VALUE){
+            out = -1;
+        }
+        
+        return out;
     }
 }
