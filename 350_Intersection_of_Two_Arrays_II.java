22fc22887bce3f12f04a468fbc25b6c0639f21da
jerrychlee <jerrychlee@gmail.com>
2019-03-12 11:09:53 +0800

    HashMap & ArrayList

--- 350_Intersection_of_Two_Arrays_II.java
+++ 350_Intersection_of_Two_Arrays_II.java
@@ -1,32 +1,35 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        List<Integer> out = new ArrayList();
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        ArrayList<Integer> aList = new ArrayList<Integer>(); 
+
         for(int num:nums1){
             if(map.containsKey(num)){
-                map.put(num, map.get(num)+1);
+                map.put(num,map.get(num)+1);            
             }else{
-                map.put(num, 1);
+                map.put(num,1);                            
             }
         }
-        
+
         for(int num:nums2){
-            int val = map.getOrDefault(num, 0);
-            if(val!=0){
-                out.add(num);
-                if(val==1){
-                    map.remove(num);
+            if(map.containsKey(num)){
+                int c = map.get(num);
+                if(c==1){
+                    map.remove(num);   
                 }else{
-                    map.put(num, val-1);
+                    map.replace(num,c-1);   
                 }
+                aList.add(num);
             }
         }
+
+      
+        int[] out = new int[aList.size()];
         
-        int[] n = new int[out.size()];
-        for(int i=0; i<out.size(); i++){
-            n[i] = out.get(i);
+        for(int i=0; i<aList.size(); i++){
+            out[i] = aList.get(i);
         }
         
-        return n;
+        return out;
     }
 }
